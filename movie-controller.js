cb7ca92ceeb18a405aaaf7f9d31bf0020d4c837d
curtjenk <curtjenk@comcast.net>
2016-03-29 13:10:34 -0400

    Initial

---
+++ movie-controller.js
@@ -0,0 +1,21 @@
+var movieApp = angular.module('movieApp', []);
+movieApp.controller('movieController', function($scope, $http) {
+
+    var baseURL = 'https://api.themoviedb.org/3/';
+    var methodNowPlaying = 'movie/now_playing';
+    var apiKey = 'api_key=2d9e9aacbdc397236dad8339b3d2c17c';
+
+    var URL = baseURL + methodNowPlaying + '?' + apiKey;
+
+    $scope.imagesArr = [];
+    
+    $http.get(URL).success(function(data) {
+
+        console.log(data);
+        for (i = 0; i < data.results.length; i++) {
+
+            $scope.imagesArr.push('http://image.tmdb.org/t/p/w300' + data.results[i].poster_path);
+        }
+    });
+
+});
